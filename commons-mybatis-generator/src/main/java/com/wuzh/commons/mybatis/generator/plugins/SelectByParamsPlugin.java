e7b53c5366e31c178e626306754d53251ee481f3
wywuzh <wywuzh@163.com>
2019-01-26 18:46:51 +0800

    SelectByParams插件完善，支持total、list、pager三种结果集查询

--- commons-mybatis-generator/src/main/java/com/wuzh/commons/mybatis/generator/plugins/SelectByParamsPlugin.java
+++ commons-mybatis-generator/src/main/java/com/wuzh/commons/mybatis/generator/plugins/SelectByParamsPlugin.java
@@ -18,20 +18,43 @@ package com.wuzh.commons.mybatis.generator.plugins;
 import com.itfsw.mybatis.generator.plugins.utils.BasePlugin;
 import com.itfsw.mybatis.generator.plugins.utils.FormatTools;
 import com.itfsw.mybatis.generator.plugins.utils.JavaElementGeneratorTools;
+import com.itfsw.mybatis.generator.plugins.utils.XmlElementGeneratorTools;
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
+
+import java.util.List;
 
 /**
  * 类QueryPlugin的实现描述：自定义select查询插件
  *
  * @author <a href="mailto:wywuzh@163.com">伍章红</a> 2019/1/25 21:48
- * @version v3.0.0
+ * @version v2.1.1
  * @since JDK 1.8
  */
 public class SelectByParamsPlugin extends BasePlugin {
+    /**
+     * 数据总数查询
+     */
     public static final String METHOD_SELECT_TOTAL = "selectTotalByParams";
+    /**
+     * 数据列表查询
+     */
     public static final String METHOD_SELECT_LIST = "selectListByParams";
+    /**
+     * 数据列表查询（分页）
+     */
     public static final String METHOD_SELECT_PAGER = "selectPagerByParams";
+    /**
+     * where条件SQL
+     */
+    public static final String WHERE_CONDITION = "appendConditions";
 
     /**
      * Java Client Methods 生成
@@ -90,5 +113,103 @@ public class SelectByParamsPlugin extends BasePlugin {
         FormatTools.addMethodWithBestPosition(interfaze, mSelectPager);
         logger.debug("selectPagerByParams插件:" + interfaze.getType().getShortName() + "增加selectPagerByParams方法。");
         return true;
+    }
+
+    /**
+     * SQL Map Methods 生成
+     * 具体执行顺序 http://www.mybatis.org/generator/reference/pluggingIn.html
+     *
+     * @param document
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        document.getRootElement().addElement(generateWhereConditionsElement(introspectedTable));
+
+        // 1. selectTotalByParams
+        XmlElement selectTotalEle = new XmlElement("select");
+        // xml节点设置唯一ID
+        selectTotalEle.addAttribute(new Attribute("id", METHOD_SELECT_TOTAL));
+        selectTotalEle.addAttribute(new Attribute("resultType", "java.lang.Long"));
+        // 参数类型
+        selectTotalEle.addAttribute(new Attribute("parameterType", "map"));
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(selectTotalEle);
+        selectTotalEle.addElement(new TextElement("select count(1)"));
+        selectTotalEle.addElement(new TextElement("from " + introspectedTable.getFullyQualifiedTableNameAtRuntime()));
+        selectTotalEle.addElement(new TextElement("where 1=1"));
+        // 引入where条件
+        XmlElement includeConditionsEle = new XmlElement("include");
+        includeConditionsEle.addAttribute(new Attribute("refid", WHERE_CONDITION));
+        selectTotalEle.addElement(includeConditionsEle);
+        document.getRootElement().addElement(selectTotalEle);
+
+        // 2. selectListByParams
+        XmlElement selectListEle = new XmlElement("select");
+        // xml节点设置唯一ID
+        selectListEle.addAttribute(new Attribute("id", METHOD_SELECT_LIST));
+        selectListEle.addAttribute(new Attribute("resultMap", introspectedTable.getBaseResultMapId()));
+        // 参数类型
+        selectListEle.addAttribute(new Attribute("parameterType", "map"));
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(selectListEle);
+        selectListEle.addElement(new TextElement("select "));
+        selectListEle.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
+        selectListEle.addElement(new TextElement("from " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+        selectListEle.addElement(new TextElement("where 1=1"));
+        selectListEle.addElement(includeConditionsEle);
+        document.getRootElement().addElement(selectListEle);
+
+        // 3. selectListByParams
+        XmlElement selectPagerEle = new XmlElement("select");
+        // xml节点设置唯一ID
+        selectPagerEle.addAttribute(new Attribute("id", METHOD_SELECT_PAGER));
+        selectPagerEle.addAttribute(new Attribute("resultMap", introspectedTable.getBaseResultMapId()));
+        // 参数类型
+        selectPagerEle.addAttribute(new Attribute("parameterType", "map"));
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(selectPagerEle);
+        selectPagerEle.addElement(new TextElement("select "));
+        selectPagerEle.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
+        selectPagerEle.addElement(new TextElement("from " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+        selectPagerEle.addElement(new TextElement("where 1=1"));
+        selectPagerEle.addElement(includeConditionsEle);
+        // 添加分页SQL
+        selectPagerEle.addElement(new TextElement("limit #{offset}, #{pageSize}"));
+        document.getRootElement().addElement(selectPagerEle);
+        return true;
+    }
+
+    /**
+     * 生成Where查询条件
+     *
+     * @param introspectedTable
+     * @return
+     */
+    private XmlElement generateWhereConditionsElement(IntrospectedTable introspectedTable) {
+        XmlElement conditionsElement = new XmlElement("sql");
+        conditionsElement.addAttribute(new Attribute("id", WHERE_CONDITION));
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(conditionsElement);
+
+        // 第一步：先判断map是否为空
+        XmlElement ifElement = new XmlElement("if");
+        ifElement.addAttribute(new Attribute("test", "map != null"));
+
+        // 获取到table中的所有column
+        List<IntrospectedColumn> introspectedColumnList = ListUtilities.removeIdentityAndGeneratedAlwaysColumns(introspectedTable.getAllColumns());
+        for (IntrospectedColumn introspectedColumn : introspectedColumnList) {
+            // 第二步：添加map中key的空判断
+            XmlElement mapKeyIfElement = new XmlElement("if");
+            mapKeyIfElement.addAttribute(new Attribute("test", introspectedColumn.getJavaProperty("map.") + " != null"));
+            // 添加column字段查询条件SQL（这里默认给表的所有字段添加and条件）
+            mapKeyIfElement.addElement(new TextElement("and " + MyBatis3FormattingUtilities.getAliasedEscapedColumnName(introspectedColumn)
+                    + " = " + MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "map.")));
+            ifElement.addElement(mapKeyIfElement);
+        }
+
+        conditionsElement.addElement(ifElement);
+        return conditionsElement;
     }
 }
