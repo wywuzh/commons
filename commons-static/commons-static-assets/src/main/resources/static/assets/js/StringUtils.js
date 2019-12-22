358b200fe5e472c89a066ea011171e066221c45d
wywuzh <wywuzh@163.com>
2019-12-22 13:31:43 +0800

    添加commons-static-assets模块

---
+++ commons-static/commons-static-assets/src/main/resources/static/assets/js/StringUtils.js
@@ -0,0 +1,39 @@
+/**
+ * String工具类
+ *
+ * @author wywuzh@163.com 2019-01-24
+ * @version v3.0.0
+ * @requires jQuery
+ */
+var StringUtils = {
+    /**
+     * 增加formatString功能
+     *
+     * 使用方法：formatString('字符串{0}字符串{1}字符串','第一个变量','第二个变量');
+     *
+     * @returns 格式化后的字符串
+     */
+    formatString: function (str) {
+        for (var i = 0; i < arguments.length - 1; i++) {
+            str = str.replace("{" + i + "}", arguments[i + 1]);
+        }
+        return str;
+    },
+    /**
+     * 接收一个以逗号分割的字符串，返回List，list里每一项都是一个字符串
+     *
+     * @returns list
+     */
+    stringToList: function (value) {
+        if (value != undefined && value != '') {
+            var values = [];
+            var t = value.split(',');
+            for (var i = 0; i < t.length; i++) {
+                values.push('' + t[i]);/* 避免他将ID当成数字 */
+            }
+            return values;
+        } else {
+            return [];
+        }
+    }
+};
