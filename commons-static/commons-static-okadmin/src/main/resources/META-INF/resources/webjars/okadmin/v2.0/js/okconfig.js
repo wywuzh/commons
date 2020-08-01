53f3c944d93fd02f11bff0412a5ebe1729079d89
wywuzh <wywuzh@163.com>
2020-08-01 22:43:21 +0800

    ok-admin升级至20200729

--- commons-static/commons-static-okadmin/src/main/resources/META-INF/resources/webjars/okadmin/v2.0/js/okconfig.js
+++ commons-static/commons-static-okadmin/src/main/resources/META-INF/resources/webjars/okadmin/v2.0/js/okconfig.js
@@ -1,6 +1,10 @@
 var okConfig = {
-   theme: "blue_theme", //主题色orange_theme|blue_theme
-   menuArrow: "ok-arrow2", //导航箭头ok-arrow2|ok-arrow3,不填为默认样式
-   isTabMenu: true,  //刷新后是否记住上次打开tab菜单
-   isTabRefresh: false, //是否开启切换刷新
+    // 主题色orange_theme|blue_theme
+    theme: "blue_theme",
+    // 导航箭头ok-arrow2|ok-arrow3,不填为默认样式
+    menuArrow: "ok-arrow2",
+    //刷新后是否记住上次打开tab菜单
+    isTabMenu: true,
+    // 是否开启切换刷新
+    isTabRefresh: false
 };
