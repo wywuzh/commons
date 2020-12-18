e5578d688cebae694a4d00932384890cc0e86f46
wywuzh <wywuzh@163.com>
2020-12-18 19:23:49 +0800

    MBG工具优化

--- commons-mybatis-generator/src/main/java/com/wuzh/commons/mybatis/generator/plugins/SelectByParamsPlugin.java
+++ commons-mybatis-generator/src/main/java/com/wuzh/commons/mybatis/generator/plugins/SelectByParamsPlugin.java
@@ -43,6 +43,45 @@ import java.util.List;
  * @since JDK 1.8
  */
 public class SelectByParamsPlugin extends BasePlugin {
+
+    /**
+     * 表是否开启物理删除
+     *
+     * @since 2.3.6
+     */
+    public static final String ENABLE_PHYSICAL_DELETE = "enablePhysicalDelete";
+    /**
+     * 物理删除字段。在生成selectTotalByParams、selectListByParams、selectPagerByParams查询语句时，会在where条件后面添加该条件。
+     *
+     * @since 2.3.6
+     */
+    public static final String PHYSICAL_DELETE_FIELD = "physicalDeleteField";
+    /**
+     * 排除数据sql，即剔除已删除数据的sql
+     *
+     * @since 2.3.6
+     */
+    public static final String EXCLUDE_DELETED_SQL = "excludeDeletedSql";
+    /**
+     * 表是否开启物理删除，默认为true
+     *
+     * @since 2.3.6
+     */
+    private boolean enablePhysicalDelete = true;
+    /**
+     * 物理删除字段默认值
+     *
+     * @since 2.3.6
+     */
+    private String physicalDeleteField = "is_delete";
+    /**
+     * 排除删除数据sql。在生成selectTotalByParams、selectListByParams、selectPagerByParams查询语句时，会在where条件后面添加该条件
+     *
+     * @since 2.3.6
+     */
+    private String excludeDeletedSql = "is_delete = 0";
+
+
     /**
      * 数据总数查询
      */
@@ -68,6 +107,60 @@ public class SelectByParamsPlugin extends BasePlugin {
      * Foreach in查询
      */
     public static final String CONDITIONS_FOREACH_IN_COLUMNS = "conditionsForeachInColumns";
+
+    @Override
+    public void initialized(IntrospectedTable introspectedTable) {
+        super.initialized(introspectedTable);
+
+        // v2.3.6
+        // 表是否开启物理删除，默认为true
+        String enablePhysicalDelete = super.getProperties().getProperty(ENABLE_PHYSICAL_DELETE);
+        if (StringUtils.isNotBlank(enablePhysicalDelete)) {
+            this.enablePhysicalDelete = Boolean.valueOf(enablePhysicalDelete);
+        }
+        // 物理删除字段
+        String physicalDeleteField = super.getProperties().getProperty(PHYSICAL_DELETE_FIELD);
+        if (StringUtils.isNotBlank(enablePhysicalDelete)) {
+            this.physicalDeleteField = physicalDeleteField;
+        }
+        // 排除数据sql，即剔除已删除数据的sql
+        String excludeDeletedSql = super.getProperties().getProperty(EXCLUDE_DELETED_SQL);
+        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+            this.excludeDeletedSql = excludeDeletedSql;
+        }
+    }
+
+    /**
+     * 表是否开启物理删除，默认为true
+     *
+     * @param tableConfiguration table配置
+     * @return
+     * @since 2.3.6
+     */
+    private boolean enablePhysicalDelete(TableConfiguration tableConfiguration) {
+        // 如果在<table>中有配置，以该配置为准，否则读取全局配置
+        String enablePhysicalDelete = tableConfiguration.getProperty(ENABLE_PHYSICAL_DELETE);
+        if (StringUtils.isNotBlank(enablePhysicalDelete)) {
+            return Boolean.valueOf(enablePhysicalDelete);
+        }
+        return this.enablePhysicalDelete;
+    }
+
+    /**
+     * 排除删除数据sql。在生成selectTotalByParams、selectListByParams、selectPagerByParams查询语句时，会在where条件后面添加该条件
+     *
+     * @param tableConfiguration table配置
+     * @return
+     * @since 2.3.6
+     */
+    private String excludeDeletedSql(TableConfiguration tableConfiguration) {
+        // 如果在<table>中有配置，以该配置为准，否则读取全局配置
+        String excludeDeletedSql = tableConfiguration.getProperty(EXCLUDE_DELETED_SQL);
+        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+            return excludeDeletedSql;
+        }
+        return this.excludeDeletedSql;
+    }
 
     /**
      * Java Client Methods 生成
@@ -115,7 +208,8 @@ public class SelectByParamsPlugin extends BasePlugin {
         // 查询接口返回数据
         String driverClass = this.getContext().getJdbcConnectionConfiguration().getDriverClass();
         Parameter secondParameter = new Parameter(new FullyQualifiedJavaType("java.lang.Integer"), "pageSize", "@Param(\"pageSize\")");
-        if ("oracle.jdbc.driver.OracleDriver".equalsIgnoreCase(driverClass) == true) {
+        if (DRIVER_ORACLE_OLD.equalsIgnoreCase(driverClass)
+                || DRIVER_ORACLE.equalsIgnoreCase(driverClass)) {
             secondParameter = new Parameter(new FullyQualifiedJavaType("java.lang.Integer"), "endset", "@Param(\"endset\")");
         }
         Method mSelectPager = JavaElementGeneratorTools.generateMethod(
@@ -143,6 +237,11 @@ public class SelectByParamsPlugin extends BasePlugin {
      */
     @Override
     public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+//        Properties properties = super.getProperties();
+        // 排除数据sql，即剔除已删除数据的sql
+//        String excludeDeletedSql = properties.getProperty(EXCLUDE_DELETED_SQL, this.excludeDeletedSql);
+//        String excludeDeletedSql = excludeDeletedSql(introspectedTable.getTableConfiguration());
+
         // 1. selectTotalByParams
         XmlElement selectTotalEle = new XmlElement("select");
         // xml节点设置唯一ID
@@ -154,7 +253,9 @@ public class SelectByParamsPlugin extends BasePlugin {
         commentGenerator.addComment(selectTotalEle);
         selectTotalEle.addElement(new TextElement("select count(1)"));
         selectTotalEle.addElement(new TextElement("from " + introspectedTable.getFullyQualifiedTableNameAtRuntime()));
-//        selectTotalEle.addElement(new TextElement("where 1=1"));
+//        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+//            selectTotalEle.addElement(new TextElement("where " + excludeDeletedSql));
+//        }
         // 引入where条件
         XmlElement includeConditionsEle = new XmlElement("include");
         includeConditionsEle.addAttribute(new Attribute("refid", WHERE_CONDITION));
@@ -173,7 +274,9 @@ public class SelectByParamsPlugin extends BasePlugin {
         selectListEle.addElement(new TextElement("select "));
         selectListEle.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
         selectListEle.addElement(new TextElement("from " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
-//        selectListEle.addElement(new TextElement("where 1=1"));
+//        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+//            selectListEle.addElement(new TextElement("where " + excludeDeletedSql));
+//        }
         selectListEle.addElement(includeConditionsEle);
         // 增加排序功能
         selectListEle.addElement(generateSortElement(introspectedTable));
@@ -189,7 +292,9 @@ public class SelectByParamsPlugin extends BasePlugin {
         // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
         commentGenerator.addComment(selectPagerEle);
 
-        if ("oracle.jdbc.driver.OracleDriver".equalsIgnoreCase(this.getContext().getJdbcConnectionConfiguration().getDriverClass())) {
+        String driverClass = this.getContext().getJdbcConnectionConfiguration().getDriverClass();
+        if (DRIVER_ORACLE_OLD.equalsIgnoreCase(driverClass)
+                || DRIVER_ORACLE.equalsIgnoreCase(driverClass)) {
             // 生成Oracle分页查询SQL
             generateOraclePager(document, introspectedTable, selectPagerEle, includeConditionsEle);
         } else {
@@ -205,9 +310,17 @@ public class SelectByParamsPlugin extends BasePlugin {
     }
 
     private void generateMySQLPager(Document document, IntrospectedTable introspectedTable, XmlElement selectPagerEle, XmlElement includeConditionsEle) {
+//        // 表是否开启物理删除，默认为true
+//        boolean enablePhysicalDelete = enablePhysicalDelete(introspectedTable.getTableConfiguration());
+//        // 排除数据sql，即剔除已删除数据的sql
+//        String excludeDeletedSql = excludeDeletedSql(introspectedTable.getTableConfiguration());
+
         selectPagerEle.addElement(new TextElement("select "));
         selectPagerEle.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
         selectPagerEle.addElement(new TextElement("from " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+//        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+//            selectPagerEle.addElement(new TextElement("where " + excludeDeletedSql));
+//        }
         // 增加where条件SQL
         selectPagerEle.addElement(includeConditionsEle);
         // 增加排序功能
@@ -217,11 +330,19 @@ public class SelectByParamsPlugin extends BasePlugin {
     }
 
     private void generateOraclePager(Document document, IntrospectedTable introspectedTable, XmlElement selectPagerEle, XmlElement includeConditionsEle) {
+//        // 表是否开启物理删除，默认为true
+//        boolean enablePhysicalDelete = enablePhysicalDelete(introspectedTable.getTableConfiguration());
+//        // 排除数据sql，即剔除已删除数据的sql
+//        String excludeDeletedSql = excludeDeletedSql(introspectedTable.getTableConfiguration());
+
         selectPagerEle.addElement(new TextElement("select * from ("));
         selectPagerEle.addElement(new TextElement("select a.*, ROWNUM rn from ("));
         selectPagerEle.addElement(new TextElement("select "));
         selectPagerEle.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
         selectPagerEle.addElement(new TextElement("from " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+//        if (StringUtils.isNotBlank(excludeDeletedSql)) {
+//            selectPagerEle.addElement(new TextElement("where " + excludeDeletedSql));
+//        }
         // 增加where条件SQL
         selectPagerEle.addElement(includeConditionsEle);
         // 增加排序功能
@@ -231,6 +352,38 @@ public class SelectByParamsPlugin extends BasePlugin {
         selectPagerEle.addElement(new TextElement("where ROWNUM &lt;= #{endset}"));
         selectPagerEle.addElement(new TextElement(")"));
         selectPagerEle.addElement(new TextElement("where rn &gt; #{offset}"));
+    }
+
+    /**
+     * 生成Where查询条件
+     *
+     * @param introspectedTable
+     * @return
+     * @since 2.3.6
+     */
+    @Deprecated
+    private XmlElement generateConditionsElement(IntrospectedTable introspectedTable) {
+        XmlElement conditionsElement = new XmlElement("sql");
+        conditionsElement.addAttribute(new Attribute("id", WHERE_CONDITION));
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(conditionsElement);
+
+        // 第一步：先判断map是否为空
+        XmlElement ifElement = new XmlElement("if");
+        ifElement.addAttribute(new Attribute("test", "map != null"));
+
+        // 排除数据sql，即剔除已删除数据的sql
+        String excludeDeletedSql = super.getProperties().getProperty(EXCLUDE_DELETED_SQL, this.excludeDeletedSql);
+        XmlElement excludeDeletedElement = new XmlElement("if");
+        excludeDeletedElement.addAttribute(new Attribute("test", "true"));
+        excludeDeletedElement.addElement(new TextElement(excludeDeletedSql));
+        ifElement.addElement(excludeDeletedElement);
+
+        // 获取到table中的所有column
+        addElementForAppendConditions(introspectedTable, ifElement);
+
+        conditionsElement.addElement(ifElement);
+        return conditionsElement;
     }
 
     /**
@@ -259,6 +412,46 @@ public class SelectByParamsPlugin extends BasePlugin {
         XmlElement whereElement = new XmlElement("where");
 
         // 获取到table中的所有column
+        addElementForAppendConditions(introspectedTable, whereElement);
+
+        ifElement.addElement(whereElement);
+
+        conditionsElement.addElement(ifElement);
+        return conditionsElement;
+    }
+
+    /**
+     * appendConditions查询条件中添加if节点
+     *
+     * @param introspectedTable
+     * @param rootElement
+     * @since 2.3.6
+     */
+    private void addElementForAppendConditions(IntrospectedTable introspectedTable, XmlElement rootElement) {
+        TableConfiguration tableConfiguration = introspectedTable.getTableConfiguration();
+
+        // 表是否开启物理删除，默认为true
+        boolean enablePhysicalDelete = enablePhysicalDelete(tableConfiguration);
+        // 排除数据sql，即剔除已删除数据的sql
+        String excludeDeletedSql = excludeDeletedSql(tableConfiguration);
+        if (enablePhysicalDelete && StringUtils.isNotBlank(excludeDeletedSql)) {
+            XmlElement excludeDeletedElement = new XmlElement("if");
+            excludeDeletedElement.addAttribute(new Attribute("test", "true"));
+            // 去掉前后空格
+            excludeDeletedSql = StringUtils.trim(excludeDeletedSql);
+            if (!StringUtils.startsWithIgnoreCase(excludeDeletedSql, "and ")) {
+                excludeDeletedSql = "and " + excludeDeletedSql;
+            }
+            excludeDeletedElement.addElement(new TextElement(excludeDeletedSql));
+
+            rootElement.addElement(excludeDeletedElement);
+        }
+
+        // 开启Like模糊查询
+        List<String> conditionsLikeColumns = getConditionsLikeColumns(tableConfiguration);
+        // 开启Foreach in查询
+        List<String> conditionsForeachInColumns = getConditionsForeachInColumns(tableConfiguration);
+
         List<IntrospectedColumn> introspectedColumnList = ListUtilities.removeIdentityAndGeneratedAlwaysColumns(introspectedTable.getAllColumns());
         for (int i = 0; i < introspectedColumnList.size(); i++) {
             IntrospectedColumn introspectedColumn = introspectedColumnList.get(i);
@@ -295,7 +488,7 @@ public class SelectByParamsPlugin extends BasePlugin {
                     JdbcType.DOUBLE.equals(jdbcType) ||
                     JdbcType.NUMERIC.equals(jdbcType) ||
                     JdbcType.DECIMAL.equals(jdbcType)) {
-                mapKeyIfElement.addAttribute(new Attribute("test", javaProperty + " != null and " + javaProperty + " != ''"));
+                mapKeyIfElement.addAttribute(new Attribute("test", javaProperty + " != null"));
             } else {
                 mapKeyIfElement.addAttribute(new Attribute("test", javaProperty + " != null"));
             }
@@ -305,27 +498,23 @@ public class SelectByParamsPlugin extends BasePlugin {
                 mapKeyIfElement.addElement(new TextElement("and " + columnName
                         + " = " + MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "map.")));
             } else {
-                mapKeyIfElement.addElement(new TextElement(columnName
+                mapKeyIfElement.addElement(new TextElement("and " + columnName
                         + " = " + MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, "map.")));
             }
-            whereElement.addElement(mapKeyIfElement);
+            rootElement.addElement(mapKeyIfElement);
 
             // 添加 like 模糊查询支持
             if (conditionsLikeColumns != null && conditionsLikeColumns.size() > 0 &&
                     conditionsLikeColumns.contains(columnName)) {
-                addElementForLike(whereElement, introspectedColumn, columnName, javaProperty + "Like");
+                addElementForLike(rootElement, introspectedColumn, columnName, javaProperty + "Like");
             }
 
             // 添加 foreach in 查询支持
             if (conditionsForeachInColumns != null && conditionsForeachInColumns.size() > 0 &&
                     conditionsForeachInColumns.contains(columnName)) {
-                addElementForIn(whereElement, introspectedColumn, columnName, javaProperty + "s");
+                addElementForIn(rootElement, introspectedColumn, columnName, javaProperty + "s");
             }
         }
-        ifElement.addElement(whereElement);
-
-        conditionsElement.addElement(ifElement);
-        return conditionsElement;
     }
 
     /**
@@ -362,7 +551,8 @@ public class SelectByParamsPlugin extends BasePlugin {
 
         javaProperty = "#{" + javaProperty + "}";
         String driverClass = this.getContext().getJdbcConnectionConfiguration().getDriverClass();
-        if ("oracle.jdbc.driver.OracleDriver".equalsIgnoreCase(driverClass)) {
+        if (DRIVER_ORACLE_OLD.equalsIgnoreCase(driverClass)
+                || DRIVER_ORACLE.equalsIgnoreCase(driverClass)) {
             // 生成Oracle模糊查询SQL
             String likeSql = " like '%'||" + javaProperty + "||'%'";
             mapKeyIfElement.addElement(new TextElement("and " + columnName + likeSql));
