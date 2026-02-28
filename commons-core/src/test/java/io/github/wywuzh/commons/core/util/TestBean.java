8fa222721b78b2de85a7dfb98919d7c68cd411a8
wywuzh <wywuzh@163.com>
2026-02-28 11:42:48 +0800

    ConvertUtil工具优化

---
+++ commons-core/src/test/java/io/github/wywuzh/commons/core/util/TestBean.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.wywuzh.commons.core.util;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * 测试用的JavaBean类
+ *
+ * @author <a href="mailto:wywuzh@163.com">伍章红</a> 2026-02-28 10:26:06
+ * @version v3.5.0
+ * @since JDK 17
+ */
+@Data
+public class TestBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private Integer age;
+    private Short score;
+    private Byte level;
+    private Character grade;
+    private Double price;
+    private Float discount;
+    private String name;
+    private Boolean active;
+    private Date createDate;
+    private java.sql.Date sqlDate;
+    private Time time;
+}
