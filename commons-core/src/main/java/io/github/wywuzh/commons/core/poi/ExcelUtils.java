5591a10b7683abc39d4ec72ebed11f8e1796f8ab
wywuzh <wywuzh@163.com>
2026-04-27 10:13:35 +0800

    1、ExcelUtils工具：补充中文注释 2、JsonMapper工具优化

--- commons-core/src/main/java/io/github/wywuzh/commons/core/poi/ExcelUtils.java
+++ commons-core/src/main/java/io/github/wywuzh/commons/core/poi/ExcelUtils.java
@@ -917,6 +917,7 @@ public class ExcelUtils {
             hideSheet.createRow(i).createCell(0).setCellValue(columnValidationData[i]);
         }
         // 创建名称为 {prefixName}_hidden 的命名区域，可被其他单元格引用
+        // tips：命名区域名称不能重复，否则会报“The workbook already contains this name:”的异常。如果是往已存在的模板中添加命名区域，则需要先删除已存在的命名区域(公式 -> 名称管理器)。
         Name categoryName = workbook.createName();
         categoryName.setNameName(prefixName + "_hidden");
         // 设置名称引用的公式
--- commons-core/src/test/java/io/github/wywuzh/commons/core/poi/ExcelUtilsTest.java
+++ commons-core/src/test/java/io/github/wywuzh/commons/core/poi/ExcelUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 the original author or authors.
+ * Copyright 2015-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ import io.github.wywuzh.commons.core.poi.style.CellStyleTools;
 @Slf4j
 public class ExcelUtilsTest {
 
-    //    @Before
+    // @Before
     public void init() {
         System.setProperty("font.name", "宋体");
         System.setProperty("font.height", "9");
@@ -112,12 +112,7 @@ public class ExcelUtilsTest {
 
         Map<String, String[]> columnValidation = new HashMap<>();
         String[] genders = {
-                "男", "女", "未知", "其他",
-                "man", "woman",
-                "Mees", "Naised",
-                "Kişi", "Qadın",
-                "burrë", "grua",
-                "Fir", "mná",
+                "男", "女", "未知", "其他", "man", "woman", "Mees", "Naised", "Kişi", "Qadın", "burrë", "grua", "Fir", "mná",
         };
         columnValidation.put("性别", genders);
 
